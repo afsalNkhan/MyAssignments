5b73ff13dd5f44e38427617c4adf04dd00a8e0f7
afsalNkhan <134801913+afsalNkhan@users.noreply.github.com>
2023-05-27 23:59:18 +0530

    Add files via upload
    day1

---
+++ day1/IsPrime.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+		int n=21;
+		for (int i=2; i >=n;i++)
+				if (n%i==0)
+					System.out.println("prime");
+		else;
+				System.out.println("non-prime");
+
+	}
+
+}
---
+++ day1/Vehicle.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Vehicle {
+
+	public static void main(String[] args) {
+		int bike=3;
+		long numplate=5797L;
+		float capacity=10.5f;
+		char logo='Y';
+String color="RED";
+System.out.println(bike);
+System.out.println(color);
+System.out.println(capacity);
+System.out.println(logo);
+System.out.println(numplate);
+	}
+
+}
